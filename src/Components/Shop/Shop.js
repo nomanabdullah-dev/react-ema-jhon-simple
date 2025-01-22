4012d3da5f84bebbe940aa344f6a651811e9af37
nomanabdullah-dev <nomanabdullah.dev@gmail.com>
2025-01-23 01:14:46 +0600

    shop page coding done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product)=>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <ul>
-                    {
-                        products.map(product => <li>{product.name}</li>)
-                    }
-                </ul>
+                {
+                    products.map(pd => 
+                        <Product 
+                            handleAddProduct={handleAddProduct}
+                            product={pd}>
+                        </Product>
+                    )
+                }
             </div>
 
             <div className="cart-container">
-                <h3>This is cart container</h3>
+                <Cart
+                    cart={cart}>
+                </Cart>
             </div>
             
         </div>
