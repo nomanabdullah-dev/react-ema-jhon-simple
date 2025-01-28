c77b0264277c1f9cffafe8db839d5c121be8e50b
nomanabdullah-dev <nomanabdullah.dev@gmail.com>
2025-01-28 21:30:34 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Shop from './Components/Shop/Shop';
+import Review from './Components/Review/Review';
+import Inventory from './Components/Inventory/Inventory';
+import NotFound from './Components/NotFound/NotFound';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import ProductDetail from './Components/ProductDetail/ProductDetail';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Routes>
+          <Route path='/shop' element={<Shop></Shop>}></Route>
+          <Route path='/review' element={<Review></Review>}></Route>
+          <Route path='/inventory' element={<Inventory></Inventory>} />
+          <Route exact path='/' element={<Shop></Shop>}></Route>
+          <Route exact path='/product/:productKey' element={<ProductDetail></ProductDetail>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,25 +2,26 @@ import React from 'react';
 import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router';
 
 const Product = (props) => {
-    const {name, img, seller, price, stock} = props.product;
+    const {name, img, seller, price, stock, key} = props.product;
     return (
         <div className='product'>
             <div>
                 <img src={img} alt="product-image" />
             </div>
             <div>
-                <h4 className='product-name'>{name}</h4>
+                <h4 className='product-name'><Link to={"/product/"+key}>{name}</Link></h4>
                 <br />
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
-                <button 
+                { props.showAddToCart && <button 
                     className='main-button'
                     onClick={()=>props.handleAddProduct(props.product)}>
                     <FontAwesomeIcon icon={ faShoppingCart } /> add to cart
-                </button>
+                </button>}
             </div>
         </div>
     );
